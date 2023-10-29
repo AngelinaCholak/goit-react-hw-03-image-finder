d73f9da7018461f1f6443ad57321b377b3897c9b
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-10-29 11:58:23 +0400

    hw

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles from '../styles.module.css';
 import { toast } from 'react-toastify'; 
 import 'react-toastify/dist/ReactToastify.css';
-
+import { FaSearch } from 'react-icons/fa';
 class Searchbar extends Component {
   state = {
     query: '',
@@ -27,7 +27,7 @@ class Searchbar extends Component {
       <header className={styles.Searchbar}>
         <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
           <button type="submit" className={styles.SearchFormButton}>
-            <span className={styles.SearchFormButtonLabel}>Пошук</span>
+            <FaSearch className={styles.SearchFormButtonLabel} />
           </button>
           <input
             className={styles.SearchFormInput}
